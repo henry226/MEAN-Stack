dd07ec8bf9d2c632b95d2bc80d80ac6c87bc4f35
Henry Li <henry5581984@gmail.com>
2019-08-17 20:42:01 -0400

    calling get post

--- frontend/src/app/posts/post-list/post-list.component.ts
+++ frontend/src/app/posts/post-list/post-list.component.ts
@@ -1,6 +1,7 @@
-import { Component, Input } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { Post } from '../post.model';
 import { PostsService } from '../post.service';
+import { Subscription } from 'rxjs';
 
 
 @Component({
@@ -8,8 +9,20 @@ import { PostsService } from '../post.service';
   templateUrl: './post-list.component.html',
   styleUrls: ['./post-list.component.css']
 })
-export class PostListComponent {
-  @Input() posts: Post[] = [];
+export class PostListComponent implements OnInit, OnDestroy {
+  posts: Post[] = [];
+  private postsSub: Subscription;
 
   constructor(public postsService: PostsService) {}
+
+  ngOnInit() {
+    this.posts = this.postsService.getPosts();
+    this.postsSub = this.postsService.getPostUpdateListener().subscribe((posts: Post[]) => {
+      this.posts = posts;
+    });
+  }
+
+  ngOnDestroy() {
+    this.postsSub.unsubscribe();
+  }
 }
