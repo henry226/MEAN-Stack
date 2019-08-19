c44325643ef6083eeb8142cb1f030eff81c24596
Henry Li <henry5581984@gmail.com>
2019-08-18 20:31:11 -0400

    fetch initial posts

--- backend/app.js
+++ backend/app.js
@@ -3,13 +3,23 @@ const express = require('express');
 // This is getting exported. 
 const app = express();
 
-app.use((req, res, next) => {
-    console.log('First Middleware');
-    next();
-});
-
-app.use((req, res, next) => {
-    res.send('Hello from express!');
+app.use('/api/posts', (req, res, next) => {
+    const posts = [
+        {
+            id: 'first id', 
+            title: 'first title', 
+            content: 'first content'
+        },
+        {
+            id: '2nd id', 
+            title: '2nd title', 
+            content: '2nd content'
+        }
+    ];
+    res.status(200).json({
+        message: 'Posts fetched successfully!',
+        posts: posts
+    });
 });
 
 module.exports = app;
