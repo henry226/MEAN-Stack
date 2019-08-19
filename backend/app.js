a5f03f490b6fdd69d72d0e5dc70a5b98d1dc9ab5
Henry Li <henry5581984@gmail.com>
2019-08-18 21:22:21 -0400

    CORS

--- backend/app.js
+++ backend/app.js
@@ -3,6 +3,13 @@ const express = require('express');
 // This is getting exported. 
 const app = express();
 
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Header', 'Origin, X-Requested-With, Content-Type, Accept');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PATCH, DELETE, OPTIONS');
+    next();
+});
+
 app.use('/api/posts', (req, res, next) => {
     const posts = [
         {
