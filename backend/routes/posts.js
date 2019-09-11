f21f52b49d8e6174ec1f5cfeca93ad025451aa4b
Henry Li <henry5581984@gmail.com>
2019-09-10 22:15:10 -0400

    working on Pagination on backend

--- backend/routes/posts.js
+++ backend/routes/posts.js
@@ -45,7 +45,15 @@ router.post("", multer({storage: storage }).single("image"), (req, res, next) =>
 });
 
 router.get('', (req, res, next) => {
-    Post.find()
+    const pageSize = +req.query.pageSize;
+    const currentPage = +req.query.page;
+    const postQuery = Post.find();
+    if(pageSize && currentPage) {
+        postQuery
+            .skip(pageSize * (currentPage -1))
+            .limit(pageSize);
+    }
+    postQuery
         .then(documents => {
             res.status(200).json({
                 message: 'Posts fetched successfully!',
