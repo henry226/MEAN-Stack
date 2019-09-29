594faddef779aad3f42358b1fb28954fec006a16
Henry Li <henry5581984@gmail.com>
2019-09-29 17:58:10 -0400

    SPA Authentication

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const User = require("../model/user");
 const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
 
 const router = express.Router();
 
@@ -24,6 +25,34 @@ router.post("/signup", (req, res, next) => {
                     error: err
                 });
             });
+    });
+});
+
+router.post("login", (req, res, next) => {
+    User.findOne({email: req.body.email}).then(user =>{
+        if(!user) {
+            return res.status(401).json({
+                message: "Auth Failed"
+            });
+        }
+        return bcrypt.compare(req.body.password, user.password);
+    })
+    .then(result => {
+        if(!result) {
+            return res.status(401).json({
+                message: "Auth Failed"
+            });
+        }
+        const token = jwt.sign(
+            {email: user.email, userId: user._id},
+            "secret_this_should_be_longer",
+            {expiresIn: "1h"}
+        );
+    })
+    .catch(err => {
+        return res.status(401).json({
+            message: "Auth Failed"
+        });
     });
 });
 
