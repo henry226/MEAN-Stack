060352f728d5243b2b50db3e40013cb5bcf11fca
Henry Li <henry5581984@gmail.com>
2019-09-29 23:24:03 -0400

    adding middleware to protect routes

--- backend/routes/posts.js
+++ backend/routes/posts.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router = express.Router();
 const Post = require('../model/post');
 const multer = require("multer");
+const checkAuth = require("../middleware/check-auth");
 
 const MIME_TYPE_MAP = {
     'image/png': 'png',
@@ -25,14 +26,13 @@ const storage = multer.diskStorage({
     }
 });
 
-router.post("", multer({storage: storage }).single("image"), (req, res, next) => {
+router.post("", checkAuth, multer({storage: storage }).single("image"), (req, res, next) => {
     const url = req.protocol + '://' + req.get("host");
     const post = new Post({
         title: req.body.title,
         content: req.body.content,
         imagePath: url + "/images/" + req.file.filename
     });
-    //console.log(post);
     post.save().then(result => {
         res.status(201).json({
             message: 'Post added successful.',
@@ -68,14 +68,14 @@ router.get('', (req, res, next) => {
         });
 });
 
-router.delete("/:id", (req, res, next) => {
+router.delete("/:id", checkAuth, (req, res, next) => {
     Post.deleteOne({_id: req.params.id}).then(result => {
         console.log(result);
         res.status(200).json({ message: "Post Deleted!"});
     });
 });
 
-router.put("/:id", multer({storage: storage }).single("image"), (req, res, next) => {
+router.put("/:id", checkAuth, multer({storage: storage }).single("image"), (req, res, next) => {
     let imagePath = req.body.imagePath;
     if(req.file) {
         const url = req.protocol + '://' + req.get("host");
