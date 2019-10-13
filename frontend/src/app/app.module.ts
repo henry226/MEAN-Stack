5aaf51f73f8196331974d0d57d3227e1fc1e7c9b
Henry Li <henry5581984@gmail.com>
2019-10-12 23:48:16 -0400

    add token to authenticate request

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -18,10 +18,11 @@ import { PostsComponent } from './posts/posts.component';
 import { PostCreateComponent } from './posts/post-create/post-create.component';
 import { HeaderComponent } from './header/header.component';
 import { PostListComponent } from './posts/post-list/post-list.component';
-import { HttpClientModule } from '@angular/common/http';
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 import { AuthComponent } from './auth/auth.component';
 import { LoginComponent } from './auth/login/login.component';
 import { SignupComponent } from './auth/signup/signup.component';
+import { AuthInterceptor } from './auth/auth-interceptor';
 
 @NgModule({
   declarations: [
@@ -49,7 +50,7 @@ import { SignupComponent } from './auth/signup/signup.component';
     MatPaginatorModule,
     FormsModule
   ],
-  providers: [],
+  providers: [{provide: HTTP_INTERCEPTORS, useClass: AuthInterceptor, multi: true}],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- frontend/src/app/auth/auth.service.ts
+++ frontend/src/app/auth/auth.service.ts
@@ -4,8 +4,13 @@ import { AuthData } from './auth-data.model';
 
 @Injectable({ providedIn: 'root'})
 export class AuthService {
+  private token: string;
 
   constructor(private http: HttpClient) {}
+
+  getToken() {
+    return this.token;
+  }
 
   createUser(email: string, password: string) {
     const authData: AuthData = {email: email, password: password};
@@ -17,9 +22,10 @@ export class AuthService {
 
   login(email: string, password: string) {
     const authData: AuthData = {email: email, password: password};
-    this.http.post("http://localhost:3000/api/user/login", authData)
+    this.http.post<{token: string}>('http://localhost:3000/api/user/login', authData)
       .subscribe(res => {
-        console.log(res);
-      })
+        const token = res.token;
+        this.token = token;
+      });
   }
 }
