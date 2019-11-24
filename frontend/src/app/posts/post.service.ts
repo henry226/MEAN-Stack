50bdbd05cf6c0028fbb21256a1cd2aaa75122dcd
Henry Li <henry5581984@gmail.com>
2019-11-24 15:41:51 -0500

    using the user ID on the frontend

--- frontend/src/app/posts/post.service.ts
+++ frontend/src/app/posts/post.service.ts
@@ -41,19 +41,24 @@ export class PostsService {
   }
 
   getPost(id: string) {
-    return this.http.get<{_id: string, title: string, content: string, imagePath: string }>('http://localhost:3000/api/posts/' + id);
+    return this.http.get<{
+      _id: string,
+      title: string,
+      content: string,
+      imagePath: string,
+      creator: string}>('http://localhost:3000/api/posts/' + id);
   }
 
   updatePost(id: string, title: string, content: string, image: File | string) {
     let postData: Post | FormData;
     if (typeof(image) === 'object') {
       postData = new FormData();
-      postData.append("id", id);
+      postData.append('id', id);
       postData.append('title', title);
       postData.append('content', content);
       postData.append('image', image, title);
     } else {
-      postData = {id: id, title: title, content: content, imagePath: image};
+      postData = {id: id, title: title, content: content, imagePath: image, creator: null};
     }
     this.http.put('http://localhost:3000/api/posts/' + id, postData)
              .subscribe(res => {
